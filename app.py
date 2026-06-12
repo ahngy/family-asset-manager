1981b096ab3a2bcb4bd9c6e4d96fce6041ce0faf
ahngy <ahngy.gnu@gmail.com>
2026-06-12 15:25:04 +0900

    feat: account list

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import streamlit as st
 
+from utils.sheets import get_accounts
+
 st.set_page_config(
     page_title="우리집 자산관리",
     page_icon="🏠",
-    layout="wide"
+    layout="wide",
 )
 
 st.title("🏠 우리집 자산관리")
 
-col1, col2, col3 = st.columns(3)
+st.subheader("계좌 목록")
 
-with col1:
-    st.metric("총 자산", "0원")
+df = get_accounts()
 
-with col2:
-    st.metric("총 부채", "0원")
-
-with col3:
-    st.metric("순자산", "0원")
-
-st.divider()
-
-st.subheader("최근 거래")
-
-st.info("아직 등록된 거래가 없습니다.")
+st.dataframe(
+    df,
+    use_container_width=True,
+    hide_index=True,
+)
---
+++ utils/sheets.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+
+@st.cache_resource
+def get_client():
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=SCOPES,
+    )
+    return gspread.authorize(creds)
+
+
+def get_accounts():
+    client = get_client()
+
+    sheet = client.open("FamilyFinanceDB").worksheet("Accounts")
+
+    data = sheet.get_all_records()
+
+    return pd.DataFrame(data)
